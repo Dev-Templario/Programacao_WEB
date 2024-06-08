aa1f7d40c9833bcc1a597484e1a33963305b54b7
Dev Aurelio <jose@jose.com>
2024-06-08 00:12:58 -0300

    Adicionando a aula 2

--- 3-Javascript/Aula 02 - Tipos de Saída/js/index.js
+++ 3-Javascript/Aula 02 - Tipos de Saída/js/index.js
@@ -1,1 +1,3 @@
-alert("!!!!!EU NÃO AGUENTO MAIS VIVER SEM TAPIOCA!!!!!");
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JavaScript</b>!";//Primeira maneira de uma saida de dados
+alert("Oi, isso é um alerta")//Terceira maneira de uma saida de dados
+console.log("Oi, isso é um console.log")//Quarta maneira de uma saida de dados
